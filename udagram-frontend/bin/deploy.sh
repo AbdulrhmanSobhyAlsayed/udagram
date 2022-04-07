7e8cc67499dee99a99a8a1c80ddacb696471fced
AbdulrhmanSobhyAlsayed <asobalsayed@gmail.com>
2022-04-07 16:31:50 +0300

    aws s3 cp --recursive --acl public-read ./www s3://udagram-fn/

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,6 +1,6 @@
-aws s3api create-bucket \
-    --bucket udagram-fn \
-    --region us-east-1 \
-    --create-bucket-configuration LocationConstraint=us-east-1
+# aws s3api create-bucket \
+#     --bucket udagram-fn \
+#     --region us-east-1 \
+#     --create-bucket-configuration LocationConstraint=us-east-1
  
 aws s3 cp --recursive --acl public-read ./www s3://udagram-fn/

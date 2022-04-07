b71eecd53488799c478418db7f9fb7950b0be266
AbdulrhmanSobhyAlsayed <asobalsayed@gmail.com>
2022-04-07 15:37:46 +0300

    // apiHost: "http://localhost:8080/api/v0",   apiHost:     "http://learnappapi-dev.eba-ekgms9gv.us-east-1.elasticbeanstalk.com/api/v0",

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  // apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://learnappapi-dev.eba-ekgms9gv.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
